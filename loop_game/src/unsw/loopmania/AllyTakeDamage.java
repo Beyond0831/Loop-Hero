0f3137ab97157bc9d1fc85c0af5ab2d54e8dde06
Xing Shi <1114205793@qq.com>
2024-05-28 09:36:14 +0800

    modified

--- loop_game/src/unsw/loopmania/AllyTakeDamage.java
+++
@@ -1,9 +0,0 @@
-package unsw.loopmania;
-
-public interface AllyTakeDamage {
-    /**
-     * enemy do damage of given amount to ally
-     * @param amount
-     */
-    public void takeDamage(int damage);
-}
